4016b8dd92042f477fe98ac28eb486efeedf2bf3
Ana Ruy <ruyluques@hotmail.com>
2026-02-21 00:50:53 -0300

    Update project

--- main.py
+++ main.py
@@ -3,18 +3,14 @@ from src.transform import transformar_dados
 from src.load import salvar_dados
 
 def run_pipeline():
-    # Caminhos (ajuste o nome do arquivo se necessário)
     input_path = "data/raw/ecommerce_sales_analysis.csv"
     output_path = "data/processed/vendas_processadas.csv"
     
     try:
-        # Step 1: Extract
         df_raw = ler_dados(input_path)
         
-        # Step 2: Transform
         df_clean = transformar_dados(df_raw)
         
-        # Step 3: Load
         salvar_dados(df_clean, output_path)
         
         print("\n🚀 Pipeline executado com sucesso!")
